4983e741fbdc794b1f1303a9677a664c4ca5fb4e
epent <pentium95@mail.ru>
2021-11-10 12:51:58 +0200

    add get request to IP API

--- app.js
+++ app.js
@@ -4,10 +4,28 @@ const searchButton = document.getElementsByTagName("button");
 console.log(searchButton);
 const enteredIP = document.querySelector("input");
 const ipAddress = document.querySelectorAll(".output-field__output")[0];
+const locationAddress = document.querySelectorAll(".output-field__output")[1];
+const timeZone = document.querySelectorAll(".output-field__output")[2];
+const isp = document.querySelectorAll(".output-field__output")[3];
 
 function searchCity() {
-  console.log(enteredIP.value);
-  ipAddress.textContent = enteredIP.value;
+  let api_key = "at_iQOJlWwcqia8VYuVYdzrFPn1WzAYY";
+  let ip = enteredIP.value.toString();
+
+  const fetchData = async () => {
+    const response = await fetch(
+      `https://geo.ipify.org/api/v2/country?apiKey=${api_key}&ipAddress=${ip}`
+    );
+    const fetchedData = await response.json();
+    console.log(fetchedData);
+
+    ipAddress.textContent = fetchedData.ip;
+    locationAddress.textContent = `${fetchedData.location.region}, ${fetchedData.location.country}`;
+    timeZone.textContent = `UTC${fetchedData.location.timezone}`;
+    isp.textContent = fetchedData.isp;
+  };
+  fetchData();
+
   enteredIP.value = "";
 }
 
