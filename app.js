379b0af93a8d09ecbe3631945f78ab4952457b29
epent <pentium95@mail.ru>
2021-11-10 13:41:47 +0200

    add Leaflet API

--- app.js
+++ app.js
@@ -27,6 +27,22 @@ function searchCity() {
   fetchData();
 
   enteredIP.value = "";
+  console.log(window.location);
 }
 
 searchButton[0].addEventListener("click", searchCity);
+
+let mymap = L.map("map").setView([51.505, -0.09], 13);
+
+L.tileLayer(
+  "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: "mapbox/streets-v11",
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: "pk.eyJ1IjoiZXBlbnQiLCJhIjoiY2t2dGc2NjRxMGQ4YzJ3anRxNDZoNjJpeSJ9.V6jxm3rLXRVwH5tzsL9f3Q",
+  }
+).addTo(mymap);
