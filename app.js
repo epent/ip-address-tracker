25ed55fbe13b43ab126d70b85a1640ca1e08ad0f
epent <pentium95@mail.ru>
2021-11-13 20:27:50 +0200

    make the map view change on request

--- app.js
+++ app.js
@@ -8,31 +8,7 @@ const locationAddress = document.querySelectorAll(".output-field__output")[1];
 const timeZone = document.querySelectorAll(".output-field__output")[2];
 const isp = document.querySelectorAll(".output-field__output")[3];
 
-function searchCity() {
-  let api_key = "at_iQOJlWwcqia8VYuVYdzrFPn1WzAYY";
-  let ip = enteredIP.value.toString();
-
-  const fetchData = async () => {
-    const response = await fetch(
-      `https://geo.ipify.org/api/v2/country?apiKey=${api_key}&ipAddress=${ip}`
-    );
-    const fetchedData = await response.json();
-    console.log(fetchedData);
-
-    ipAddress.textContent = fetchedData.ip;
-    locationAddress.textContent = `${fetchedData.location.region}, ${fetchedData.location.country}`;
-    timeZone.textContent = `UTC${fetchedData.location.timezone}`;
-    isp.textContent = fetchedData.isp;
-  };
-  fetchData();
-
-  enteredIP.value = "";
-  console.log(window.location);
-}
-
-searchButton[0].addEventListener("click", searchCity);
-
-let mymap = L.map("map").setView([51.505, -0.09], 13);
+let mymap = L.map("map").setView([34.04915, -118.09462], 13);
 
 L.tileLayer(
   "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
@@ -43,6 +19,38 @@ L.tileLayer(
     id: "mapbox/streets-v11",
     tileSize: 512,
     zoomOffset: -1,
-    accessToken: "pk.eyJ1IjoiZXBlbnQiLCJhIjoiY2t2dGc2NjRxMGQ4YzJ3anRxNDZoNjJpeSJ9.V6jxm3rLXRVwH5tzsL9f3Q",
+    accessToken:
+      "pk.eyJ1IjoiZXBlbnQiLCJhIjoiY2t2dGc2NjRxMGQ4YzJ3anRxNDZoNjJpeSJ9.V6jxm3rLXRVwH5tzsL9f3Q",
   }
 ).addTo(mymap);
+
+let marker = L.marker([51.5, -0.09]).addTo(mymap);
+
+function searchCity() {
+  let api_key = "at_iQOJlWwcqia8VYuVYdzrFPn1WzAYY";
+  let ip = enteredIP.value.toString();
+
+  const fetchData = async () => {
+    const response = await fetch(
+      `https://geo.ipify.org/api/v2/country,city?apiKey=${api_key}&ipAddress=${ip}`
+    );
+    const fetchedData = await response.json();
+
+    ipAddress.textContent = fetchedData.ip;
+    locationAddress.textContent = `${fetchedData.location.region}, ${fetchedData.location.country}`;
+    timeZone.textContent = `UTC${fetchedData.location.timezone}`;
+    isp.textContent = fetchedData.isp;
+
+    mymap.setView([fetchedData.location.lat, fetchedData.location.lng], 13);
+
+    marker = L.marker([
+      fetchedData.location.lat,
+      fetchedData.location.lng,
+    ]).addTo(mymap);
+  };
+  fetchData();
+
+  enteredIP.value = "";
+}
+
+searchButton[0].addEventListener("click", searchCity);
